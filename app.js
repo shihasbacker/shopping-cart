b61afbb7851e418f2812c274695233cb18c9c141
shihasbacker <shihasbacker@yahoo.com>
2020-11-03 12:16:31 +0530

    login, logout added

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var fileUpload=require('express-fileupload')
 
 var db=require('./config/connection')
 
+var session=require('express-session')
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -28,6 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(fileUpload())
+app.use(session({secret:"Key",cookie:{maxAge:600000}}))
 
 db.connect((err)=>{
   if(err) console.log("Connection error"+err);
